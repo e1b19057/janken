63b00ae8868bf6f4064a1235e986340c85906ab7
e1b19057 <e1b19057@oit.ac.jp>
2021-09-28 15:25:59 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z0846/kaizi/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0846.kaizi.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
